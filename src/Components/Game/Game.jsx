feaae39144c16dc581f910c76854f519237ad60b
Sinde Correia <sindecorreia@gmail.com>
2023-12-06 20:26:05 +0000

    .

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import SuperMariopng from "./SuperMario.jpg"
 import Quiz from "./quizz.jpg"
 import Simon from "./Simon.png"
@@ -87,8 +88,8 @@ function Game() {
             <div className="card-content">
               <h2>{card.topText}</h2>
               <div className="button-container">
-           <a href={card.routes}> <button className="Play-button">{card.button1Text}</button> </a>
-            <a href={card.url} rel="noopener noreferrer" target="_blank"><button className="Code-button">{card.button2Text}</button></a>
+           <Link to={card.routes}> <button className="Play-button">{card.button1Text}</button> </Link>
+            <Link to={card.url} rel="noopener noreferrer" target="_blank"><button className="Code-button">{card.button2Text}</button></Link>
               </div>
             </div>
           </div>
