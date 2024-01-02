567304217dcacf9cea318b432c3c10f156270409
Sinde Correia <sindecorreia@gmail.com>
2024-01-02 22:11:01 +0000

    comentario

--- src/App.js
+++ src/App.js
@@ -1,48 +1,50 @@
-import { BrowserRouter , Routes , Route } from 'react-router-dom';
+//import { BrowserRouter , Routes , Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './Components/NavBar/NavBar';
-import Footer from './Components/Footer/Footer';
-import Feedback from'./Components/Feedback/Feedback';
-import Help from'./Components/Help/Help';
-import Home from './Components/Home/Home';
-import Mario from './Components/Game/SuperMario/Supermario';
-import Adventure from './Components/Game/Adventure/Adventure';
-import Quiz from './Components/Game/Quiz/Quiz';
-import Simon from './Components/Game/Simon/Simon';
-import Target from './Components/Game/Target/Target';
-import Cups from './Components/Game/Cups/Cups';
+//import Navbar from './Components/NavBar/NavBar';
+//import Footer from './Components/Footer/Footer';
+//import Feedback from'./Components/Feedback/Feedback';
+//import Help from'./Components/Help/Help';
+//import Home from './Components/Home/Home';
+//import Mario from './Components/Game/SuperMario/Supermario';
+//import Adventure from './Components/Game/Adventure/Adventure';
+//import Quiz from './Components/Game/Quiz/Quiz';
+//import Simon from './Components/Game/Simon/Simon';
+//import Target from './Components/Game/Target/Target';
+//import Cups from './Components/Game/Cups/Cups';
 //import Shifter from './Components/Project/Silfter/shilfter';
-import Delivery from './Components/Project/Delivery/Delivery';
-import Calory_tracker from './Components/Project/Nutripal/Nutripal';
+//import Delivery from './Components/Project/Delivery/Delivery';
+//import Calory_tracker from './Components/Project/Nutripal/Nutripal';
 
 function App() {
   return (
     <div className="App">
-          <BrowserRouter>
-
-      <div id='NavBar'><Navbar/></div>  
-       <Routes>
-         <Route path="/feedback" element={<Feedback/>} />
-         <Route path="/Help" element={<Help/>} />
-         <Route path="/" element={<Home/>} />
-         <Route path="/Mario" element={<Mario/>} />
-         <Route path="/DeliveryWebsite" element={<Delivery/>} />
-         <Route path="/CalorieTracker" element={<Calory_tracker/>} />
-         <Route path="/Adventure" element={<Adventure/>} />
-         <Route path="/Quiz" element={<Quiz/>} />
-         <Route path="/Simon" element={<Simon/>} />
-         <Route path="/Target" element={<Target/>} />
-         <Route path="/Cups" element={<Cups/>} />
-        </Routes>
-       <Footer/>
-      </BrowserRouter>
+        aaaaa
     </div>
   )}
 
 
 export default App;
 // <Route path="/Shifter" element={<Shifter/>} />
+//<Route path="/feedback" element={<Feedback/>} />
+//<Route path="/Help" element={<Help/>} />
+//<Route path="/" element={<Home/>} />
+//<Route path="/Mario" element={<Mario/>} />
+//<Route path="/DeliveryWebsite" element={<Delivery/>} />
+//<Route path="/CalorieTracker" element={<Calory_tracker/>} />
+//<Route path="/Adventure" element={<Adventure/>} />
+//<Route path="/Quiz" element={<Quiz/>} />
+//<Route path="/Simon" element={<Simon/>} />
+//<Route path="/Target" element={<Target/>} />
+//<Route path="/Cups" element={<Cups/>} />
 
 
+//<BrowserRouter>
+
+//<div id='NavBar'><Navbar/></div>  
+ //<Routes>
+  
+  //</Routes>
+ //<Footer/>
+//</BrowserRouter>
 
 
