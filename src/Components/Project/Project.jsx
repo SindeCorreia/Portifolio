5145b4eefbfc0ec15b51d5bd8b2b6e9971a30da1
Sinde Correia <sindecorreia@gmail.com>
2024-01-02 22:01:35 +0000

    a

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import './Project.css';
 //import Shilfter from "./Shilfter.png";
 import Tracker from "./Tracker.png";
