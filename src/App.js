4806e1abba985276f02ee33320d54370a23d4a10
Sinde Correia <sindecorreia@gmail.com>
2024-01-03 12:32:10 +0000

    Y

--- src/App.js
+++ src/App.js
@@ -1,59 +1,23 @@
-import { BrowserRouter , Router , Route ,Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Teste from './Components/teste';
 import Navbar from './Components/NavBar/NavBar';
-//import Footer from './Components/Footer/Footer';
-import Feedback from'./Components/Feedback/Feedback';
-import Help from'./Components/Help/Help';
+import Feedback from './Components/Feedback/Feedback';
+import Help from './Components/Help/Help';
 import Home from './Components/Home/Home';
-import Footer from "./Components/Footer/Footer"
-//import Mario from './Components/Game/SuperMario/Supermario';
-//import Adventure from './Components/Game/Adventure/Adventure';
-//import Quiz from './Components/Game/Quiz/Quiz';
-//import Simon from './Components/Game/Simon/Simon';
-//import Target from './Components/Game/Target/Target';
-//import Cups from './Components/Game/Cups/Cups';
-//import Shifter from './Components/Project/Silfter/shilfter';
-//import Delivery from './Components/Project/Delivery/Delivery';
-//import Calory_tracker from './Components/Project/Nutripal/Nutripal';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-  <div className="App">  
-  <Router>
-    <Switch>
-    <Route path="/" element={<Home/>} />
-      <Footer/>
-    </Switch>
-</Router>
-
-
-  </div>
-  )}
-
+    <div className="App">  
+      <Router>
+        <Switch>
+          <Route path="/" element={<Home />} />
+        </Switch>
+      </Router>
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
-// <Route path="/Shifter" element={<Shifter/>} />
-//<Route path="/Mario" element={<Mario/>} />
-//<Route path="/DeliveryWebsite" element={<Delivery/>} />
-//<Route path="/CalorieTracker" element={<Calory_tracker/>} />
-//<Route path="/Adventure" element={<Adventure/>} />
-//<Route path="/Quiz" element={<Quiz/>} />
-//<Route path="/Simon" element={<Simon/>} />
-//<Route path="/Target" element={<Target/>} />
-//<Route path="/Cups" element={<Cups/>} />
-
-
-//<BrowserRouter>
-
- //<Routes>
-  
-  //</Routes>
- //<Footer/>
-//</BrowserRouter>
-
-//
-  //    <Route path="/feedback" element={<Feedback/>} />
-    //  <Route path="/Help" element={<Help/>} />
-
-
