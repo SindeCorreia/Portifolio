9dd3925405537a5c94931b92b69dbcbe64a126e3
Sinde Correia <sindecorreia@gmail.com>
2024-01-03 11:55:26 +0000

    n

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-//import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/NavBar/NavBar';
 //import Footer from './Components/Footer/Footer';
