5e06a21411a68fdd2f7444e9b33b78ef15c93fd2
Sinde Correia <sindecorreia@gmail.com>
2024-01-04 19:28:32 +0000

    teste

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Teste from './Components/teste';
 import Navbar from './Components/NavBar/NavBar';
@@ -11,7 +11,13 @@ function App() {
   return (
     <div className="App"> 
       <Navbar />
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/feed" element={<Feedback />} />
+          <Route path="/help" element={<Help />} />
+        </Routes>
+      </Router>
       <Footer />
     </div>
   );
