f5bd1f9270b9ec3e49c8f05ad655e397699052d4
Sinde Correia <sindecorreia@gmail.com>
2024-01-05 16:18:38 +0000

    novo teste

--- src/App.js
+++ src/App.js
@@ -6,13 +6,6 @@ import Feedback from './Components/Feedback/Feedback';
 import Help from './Components/Help/Help';
 import Home from './Components/Home/Home';
 import Footer from './Components/Footer/Footer';
-import Mario from './Components/Game/SuperMario/Supermario';
-import Simon from './Components/Game/Simon/Simon';
-import Cups from './Components/Game/Cups/Cups';
-import Target from './Components/Game/Target/Target';
-import Adventure from './Components/Game/Adventure/Adventure';
-import Quiz from './Components/Game/Quiz/Quiz';
-
 import './App.css';
 
 const App = () => {
