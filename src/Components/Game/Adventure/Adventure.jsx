bcaa43009c5560a5814ade0cbf518d89f504e315
Sinde Correia <sindecorreia@gmail.com>
2024-01-08 17:59:20 +0000

    circle 3

--- src/Components/Game/Adventure/Adventure.jsx
+++ src/Components/Game/Adventure/Adventure.jsx
@@ -4,9 +4,15 @@ function Adventure  () {
 return(
     <div className="AdventureGameBoard" >
     <div className="game-board">
-    <div class="circulo"></div>
+     <div class="circle">
+  <div class="slice" id="slice1"></div>
+  <div class="slice" id="slice2"></div>
+  <div class="slice" id="slice3"></div>
+</div>
+
+
     </div>
-    </div>
+  </div>
     )
  }
  export default Adventure;
